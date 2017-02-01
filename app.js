db577fd8fe25f97cacdc5b2bf52a15fff3f8ca71
trrippy <wes.wong@hotmail.com>
2017-02-01 21:17:39 +0000

    baseline filesytem

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 // Basic express setup:
 
-const PORT          = process.env.PORT || 8080;
+const PORT          = process.env.PORT || 3000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
@@ -14,6 +14,10 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
