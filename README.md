641f2927c11a07c63f09b7129b21eb3f02bf8f08
trrippy <wes.wong@hotmail.com>
2017-02-01 08:11:43 +0000

    Initial commit, added file system structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# This is the midterm project for LHL labs
+
+### We got a lot of things to do, so in no particular order:
+
+ - Role distribution (General guidelines)
+    - Front End Design
+    - Server Level
+    - Databasess
+ - File organization
+ - Brainstorming
+    - Functionality first!
+    - I think **Pathways between server and html using ReST convention (routes)** EJS here
+    - Database design (ERD), drawn up, mongodb? or should be move to sql?
+    - Website design UX
+
---
+++ server/app.js
@@ -0,0 +1,20 @@
+"use strict";
+// require('dotenv').config();
+
+// Basic express setup:
+
+const PORT          = process.env.PORT || 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+
+// set view engine to ejs
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
