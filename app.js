7381367bee4a5bb70efe410fd469a94734df29dc
trrippy <wes.wong@hotmail.com>
2017-02-01 21:44:57 +0000

    added methodoverride to server so that we can use put/delete requests

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const PORT          = process.env.PORT || 3000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const methodOverride = require('method-override')
+
+// //Example
+// <form method="POST" action="/resource?_method=DELETE">
+//   <button type="submit">Delete resource</button>
+// </form>
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // set view engine to ejs
 app.set('view engine', 'ejs');
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
