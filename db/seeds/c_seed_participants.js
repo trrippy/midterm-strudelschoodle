c1ec4cad3279c1594c14437dac323bfb3fb082c7
trrippy <wes.wong@hotmail.com>
2017-02-03 00:44:43 +0000

    added seed files for new ERD

--- db/seeds/c_seed_participants.js
+++ db/seeds/c_seed_participants.js
@@ -8,55 +8,64 @@ exports.seed = function(knex, Promise) {
           id: 1,
           event_id: 1,
           name: 'bot1',
-          email: 'bot1@email.com'
+          email: 'bot1@email.com',
+          admin: false
         },
         {
           id: 2,
           event_id: 1,
           name: 'bot2',
-          email: 'bot2@email.com'
+          email: 'bot2@email.com',
+          admin: true
         },
         {
           id: 3,
           event_id: 1,
           name: 'bot3',
-          email: 'bot3@email.com'
+          email: 'bot3@email.com',
+          admin: false
         },
         {
           id: 4,
           event_id: 2,
           name: 'bot4',
-          email: 'bot4@email.com'
+          email: 'bot4@email.com',
+          admin: true
         },
         {
           id: 5,
           event_id: 2,
           name: 'bot5',
-          email: 'bot5@email.com'
+          email: 'bot5@email.com',
+          admin: false
         },
         {
           id: 6,
           event_id: 2,
           name: 'bot6',
-          email: 'bot6@email.com'
+          email: 'bot6@email.com',
+          admin: false
        },
         {
           id: 7,
           event_id: 3,
           name: 'bot7',
-          email: 'bot7@email.com'
+          email: 'bot7@email.com',
+          admin: true
         },
         {
           id: 8,
           event_id: 3,
           name: 'bot8',
-          email: 'bot8@email.com'
+          email: 'bot8@email.com',
+          admin: false
         },
         {
           id: 9,
           event_id: 3,
           name: 'bot9',
-          email: 'bot9@email.com'
+          email: 'bot9@email.com',
+          admin: false
         }], 'id'
     ).into('participants')
   });
--- db/seeds/d_seed_availability.js
+++ db/seeds/d_seed_availability.js
@@ -7,48 +7,165 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           participant_id: 1,
-          timeslot_id: 1
+          timeslot_id: 1,
+          is_available: false
         },
         {
           id: 2,
           participant_id: 1,
-          timeslot_id: 3
+          timeslot_id: 2,
+          is_available: false
         },
         {
           id: 3,
-          participant_id: 2,
-          timeslot_id: 2
+          participant_id: 1,
+          timeslot_id: 3,
+          is_available: false
         },
         {
           id: 4,
           participant_id: 2,
-          timeslot_id: 3
+          timeslot_id: 1,
+          is_available: false
         },
         {
           id: 5,
-          participant_id: 3,
-          timeslot_id: 1
+          participant_id: 2,
+          timeslot_id: 2,
+          is_available: false
         },
         {
           id: 6,
-          participant_id: 4,
-          timeslot_id: 4
+          participant_id: 2,
+          timeslot_id: 3,
+          is_available: false
         },
         {
           id: 7,
-          participant_id: 5,
-          timeslot_id: 6
+          participant_id: 3,
+          timeslot_id: 1,
+          is_available: false
         },
         {
           id: 8,
-          participant_id: 6,
-          timeslot_id: 4
+          participant_id: 3,
+          timeslot_id: 2,
+          is_available: false
         },
         {
           id: 9,
+          participant_id: 3,
+          timeslot_id: 3,
+          is_available: false
+        },
+        {
+          id: 10,
+          participant_id: 4,
+          timeslot_id: 4,
+          is_available: false
+        },
+        {
+          id: 11,
+          participant_id: 4,
+          timeslot_id: 5,
+          is_available: false
+        },
+        {
+          id: 12,
+          participant_id: 4,
+          timeslot_id: 6,
+          is_available: false
+        },
+        {
+          id: 13,
+          participant_id: 5,
+          timeslot_id: 4,
+          is_available: false
+        },
+        {
+          id: 14,
+          participant_id: 5,
+          timeslot_id: 5,
+          is_available: false
+        },
+        {
+          id: 15,
+          participant_id: 5,
+          timeslot_id: 6,
+          is_available: false
+        },
+        {
+          id: 16,
+          participant_id: 6,
+          timeslot_id: 4,
+          is_available: false
+        },
+        {
+          id: 17,
+          participant_id: 6,
+          timeslot_id: 5,
+          is_available: false
+        },
+        {
+          id: 18,
+          participant_id: 6,
+          timeslot_id: 6,
+          is_available: false
+        },
+        {
+          id: 19,
+          participant_id: 7,
+          timeslot_id: 7,
+          is_available: false
+        },
+        {
+          id: 20,
+          participant_id: 7,
+          timeslot_id: 8,
+          is_available: false
+        },
+        {
+          id: 21,
+          participant_id: 7,
+          timeslot_id: 9,
+          is_available: false
+        },
+        {
+          id: 22,
           participant_id: 8,
-          timeslot_id: 8
-        }], 'id'
-    ).into('availability')
+          timeslot_id: 7,
+          is_available: false
+        },
+        {
+          id: 23,
+          participant_id: 8,
+          timeslot_id: 8,
+          is_available: false
+        },
+        {
+          id: 24,
+          participant_id: 8,
+          timeslot_id: 9,
+          is_available: false
+        },
+        {
+          id: 25,
+          participant_id: 9,
+          timeslot_id: 7,
+          is_available: false
+        },
+        {
+          id: 26,
+          participant_id: 9,
+          timeslot_id: 8,
+          is_available: false
+        },
+        {
+          id: 27,
+          participant_id: 9,
+          timeslot_id: 9,
+          is_available: false
+        }
+      ]).into('availability')
   });
 };
